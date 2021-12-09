b45530fce33fdf150dfeec19c589ba84bd0d33e6
aRuisu0 <90218983+aRuisu0@users.noreply.github.com>
2021-12-09 16:20:47 -0500

    Add files via upload
    Header files for our Trie and Node classes and a GUI presentation that was created to represent how our code is suppose to work.

---
+++ code/trie.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "node.h"
+#include <fstream>
+
+// Define the character size
+//#define CHAR_SIZE 128
+
+//Class to construct a trie and call methods to read in data from a file, insert a string, search for a string
+//Check for child nodes, auto completeion suggestions and auto complete prefix
+//Printing a DOT file
+class Trie {
+
+    public:
+        Node* root;
+        //bool isLeaf;
+        //int counter = 0;
+        std::ofstream output_file;
+
+        //Trie* character[CHAR_SIZE];
+
+        // Constructor
+        Trie();
+        //Destructor
+        ~Trie();
+        void readFile(std::string file_name);
+        
+        void insert(std::string data, Node* root);
+        //bool deletion(Trie const* current, std::string data);
+        
+        bool search(std::string data, Node* root);
+        
+        bool haveChildren(Node* root);
+        void auto_comp_recur(Node* root, std::string prefx);
+        int auto_comp(std::string data, Node* root);
+        void print(std::string file_name, Node* root);
+        void print_rec(Node* root, int& count);
+
+        friend class Node;
+};
